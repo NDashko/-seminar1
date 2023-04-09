399325e87ac6b83f4a7afc516659daf1e1108e23
Наталья Дашко <ndashko@mail.ru>
2023-04-09 12:45:04 +0500

    Работа над задачей завершена

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,3 +1,4 @@
-﻿Console.WriteLine("Введите число ");
+﻿//Задача на нахождение квадратного корня числа а
+Console.WriteLine("Введите число ");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine (a*a);
