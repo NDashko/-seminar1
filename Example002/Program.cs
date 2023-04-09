145924c6be6a9217014a5656b89839e8a7c2cd58
Наталья Дашко <ndashko@mail.ru>
2023-04-09 12:43:30 +0500

    Задача завершена

---
+++ Example002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число a ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b ");
+int b = int.Parse(Console.ReadLine());
+int c = b*b;
+if (a ==c)
+{
+Console.WriteLine("+");    
+}
+else
+{
+Console.WriteLine("-");   
+}
