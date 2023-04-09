504f320e195d635ed9a73ad06be26a24da47e7f0
Наталья Дашко <ndashko@mail.ru>
2023-04-09 13:16:02 +0500

    Работа над задачей завершена

---
+++ Example004/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая на вход принимает одно целое число N, а на выходе показывает все целые числа в промежутке от -N до N.
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+for (int i = -a; i<=a;  i++)
+{
+    System.Console.WriteLine(i);
+}
