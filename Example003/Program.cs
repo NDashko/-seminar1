593ffe0c6ae1428a5da2b84e3c4070dee6303d7f
Наталья Дашко <ndashko@mail.ru>
2023-04-09 13:09:33 +0500

    Добавлен сокращенный вариант

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -34,3 +34,15 @@ if (day_n >7)
 {
    Console.WriteLine("Такого дня недели нет"); 
 } 
+
+//Сокращенно это можно записать еще так:
+//Console.WriteLine("Введите день недели(число) ");
+//int day_n = int.Parse(Console.ReadLine());
+// if (day_n == 1) Console.WriteLine("Понедельник"); 
+//if (day_n == 2) Console.WriteLine("Вторник");
+//if (day_n == 3) Console.WriteLine("Среда"); 
+//if (day_n == 4) Console.WriteLine("Четверг"); 
+//if (day_n == 5) Console.WriteLine("Пятница"); 
+//if (day_n == 6) Console.WriteLine("Суббота"); 
+// if (day_n == 7)Console.WriteLine("Воскресенье"); 
+//if (day_n >7) Console.WriteLine("Такого дня недели нет"); 
